2a7a49b631f18814c35f3c0ae7b4f7523fb735ee
skdbsqls <skdbsqls98@naver.com>
2025-11-30 21:32:01 +0900

    chore: react-router-dom 설치 및 설정

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen bg-dark flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-primary">FitFinder - Home</h1>
+    </div>
+  )
+}
+
+export default HomePage
